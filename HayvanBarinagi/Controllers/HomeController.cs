c0e97a4dfda26dc2c582d0d74f55b9c6f8b03cf5
kemalbulun <94911335+kemalbulun@users.noreply.github.com>
2023-08-04 23:57:28 +0300

    1

---
+++ HayvanBarinagi/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using HayvanBarinagi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace HayvanBarinagi.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is HomeController controller &&
+                   EqualityComparer<ILogger<HomeController>>.Default.Equals(_logger, controller._logger);
+        }
+    }
+}
---
+++ HayvanBarinagi/Models/Hayvan.cs
@@ -0,0 +1,10 @@
+﻿namespace HayvanBarinagi.Models
+{
+    public class Hayvan
+    {
+        public int Id { get; set; }
+        public string Tur { get; set; }
+        public string Ad { get; set; }
+        public bool SahiplendirildiMi { get; set; }
+    }
+}
